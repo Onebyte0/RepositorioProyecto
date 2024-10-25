1ce8fb3a0dc4a42fa37e8f86e21e777a09c32926
OneByte <Blancoignacio11@gmail.com>
2024-10-24 22:11:55 -0300

    Conexion BD

--- OneByte/capaLogica/conexionBD.cs
+++ OneByte/capaLogica/conexionBD.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+
 
 namespace OneByte.capaLogica
 {
     internal class conexionBD
     {
+        
     }
 }
